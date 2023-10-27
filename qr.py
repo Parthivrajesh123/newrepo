d6e2b3ba80edf19b63dc336b8a82b2da7080d28f
Parthivrajesh123 <149141626+Parthivrajesh123@users.noreply.github.com>
2023-10-27 15:19:39 +0545

    Add files via upload

---
+++ qr.py
@@ -0,0 +1,7 @@
+cost=float(input("Enter the cost"))
+dis_per=float(input("Enter the discount in %:"))
+dis=cost*dis_per/100
+sel_per=cost-dis
+print("cost price:",cost)
+print("Discount:",dis)
+print("Selling Price:",sel_per)
