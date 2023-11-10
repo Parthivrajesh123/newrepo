7dc6c2a7195c90828534b023ff6357649c55cdd5
Parthivrajesh123 <149141626+Parthivrajesh123@users.noreply.github.com>
2023-11-10 14:58:58 +0530

    Add files via upload

---
+++ price.py
@@ -0,0 +1,7 @@
+cost=float(input("Enter the cost"))
+dis_per=float(input("input the discount in %"))
+dis=cost*dis_per/100
+sel_price=cost-dis
+print("cost price:",cost)
+print("discount:",dis)
+print("selling price:",sel_price)
